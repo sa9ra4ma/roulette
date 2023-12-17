c09b57d604270a51a2d80387545d4869d2b51f9f
sa9ra4ma <hishihishi16006@gmail.com>
2023-12-17 10:38:01 +0900

    ルーレット板追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,84 @@
     const randomIndex = Math.floor(Math.random() * weapons.length);
     weapon = weapons[randomIndex];
   }
+
+  function spinRoulette() {
+    const wheel = document.getElementById('rouletteWheel');
+    const randomDegree = Math.floor(Math.random() * 360) + 720; // Rotate between 2 to 3 full spins
+    wheel.style.transform = `rotate(${randomDegree}deg)`;
+  }
+
+  let expression: number[] = []
+  for (let i=1; i <= 100; i++) { expression.push(i); }
+  
+  function randomColor() {
+    var hex_numbers =  ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    var hex = "";
+
+    for(var i = 0; i < 6; i++) {
+      hex += hex_numbers[Math.floor(Math.random() * hex_numbers.length)];
+    }
+
+    return `#${hex}`;
+  }
+
 </script>
 
 <button on:click={roulette}>ルーレット！</button>
 <p>{ weapon }</p>
+
+<div class="roulette-container">
+  <div class="roulette-wheel" id="rouletteWheel">
+    {#each weapons as i, index (i)}
+      <div class="roulette-section" style="background-color: {randomColor()}; transform: rotate({(360/weapons.length) * (index-1)}deg)"><div class="section-text">{i}</div></div>
+    {/each}
+  </div>
+</div>
+<button class="spin-btn" on:click={spinRoulette}>Spin</button>
+
+<style>
+  .roulette-container {
+    position: relative;
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+    background-color: #f1f1f1;
+    overflow: hidden;
+  }
+
+  .roulette-wheel {
+    width: 100%;
+    height: 100%;
+    transform-origin: center;
+    transition: transform 3s ease-out;
+  }
+
+  .roulette-section {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    clip-path: polygon(50% 50%, 100% 48.4%, 100% 51.6%);
+    /* clip-path: polygon(50% 50%, 100% 0%, 100% 6%); */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .section-text {
+    color: #fff;
+    font-size: 4px;
+    font-weight: bold;
+    margin: 0 2px 0 auto;
+  }
+
+  .spin-btn {
+    margin-top: 20px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    background-color: #2ecc71;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+  }
+</style>
